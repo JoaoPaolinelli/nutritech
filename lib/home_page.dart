8fe7203d064bfb99edb4e76f468bcc1066af5311
Jean <jean.martins.a@gmail.com>
2022-12-17 13:30:06 -0300

    Organização do código da home, tela de criar receita

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,20 @@
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
+
 import 'package:flutter/material.dart';
 import 'package:nutri_tech/app_controller.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:nutri_tech/criarReceita_page.dart';
 import 'package:nutri_tech/database/Db.dart';
 import 'package:nutri_tech/models/User.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:nutri_tech/home_page.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+// import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:provider/provider.dart';
+import 'criarReceita_page.dart';
+import 'perfil_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,8 +24,6 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  File? image;
-
   late PageController controller;
   ValueNotifier<int> page = ValueNotifier(0);
 
@@ -39,23 +40,6 @@ class HomePageState extends State<HomePage> {
       duration: const Duration(milliseconds: 400),
       curve: Curves.ease,
     );
-  }
-
-  Future pickImage(source) async {
-    try {
-      if (source == null) {
-        setState(() => this.image = null);
-        return;
-      }
-
-      final image = await ImagePicker().pickImage(source: source);
-      if (image == null) return;
-
-      final imageTemporary = File(image.path);
-      setState(() => this.image = imageTemporary);
-    } on PlatformException catch (e) {
-      print('Erro ao pegar a imagem: $e');
-    }
   }
 
   int counter = 0;
@@ -105,11 +89,6 @@ class HomePageState extends State<HomePage> {
         actions: [CustomSwitch()],
       ),
       body: PageView(
-        // width: double.infinity,
-        // height: double.infinity,
-
-        // physics: PageScrollPhysics(),
-        // scrollDirection: Axis.horizontal,
         controller: controller,
         onPageChanged: (page) {
           setState(() {
@@ -117,115 +96,13 @@ class HomePageState extends State<HomePage> {
           });
         },
         children: [
+          Perfil(),
+          CriarReceita(),
           Container(
             height: 600,
             width: MediaQuery.of(context).size.width - 20,
             margin: const EdgeInsets.only(left: 10.0, right: 10.0),
             color: Color.fromARGB(5, 0, 0, 0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Column(
-                    children: [
-                      SizedBox(height: 50),
-                      ClipOval(
-                        child: Container(
-                          alignment: Alignment.center,
-                          width: 180,
-                          height: 180,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color.fromARGB(255, 204, 204, 204),
-                            border: Border.all(color: Colors.orange, width: 5),
-                          ),
-                          child: image == null
-                              ? Text(
-                                  textAlign: TextAlign.center,
-                                  "Foto",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              : Column(
-                                  children: [
-                                    Spacer(),
-                                    ClipOval(
-                                      child: Image.file(
-                                        image!,
-                                        width: 170,
-                                        height: 170,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            foregroundColor: Colors.orange,
-                            //backgroundColor: Colors.white,
-                            textStyle: TextStyle(
-                                fontSize: 10, fontWeight: FontWeight.bold),
-                          ),
-                          onPressed: () {
-                            Platform.isIOS
-                                ? showCupertinoModalPopup(
-                                    context: context,
-                                    builder: buildIOSActionSheet,
-                                  )
-                                : showModalBottomSheet(
-                                    context: context,
-                                    builder: buildAndroidActionSheet,
-                                  );
-                          },
-                          child: const Text(
-                            'editar perfil',
-                            style: TextStyle(
-                              color: Colors.orange,
-                            ),
-                          ),
-                        ),
-                      ),
-                      // TextButton(onPressed: onPressed, child: const Text('Text')),
-                      SizedBox(height: 40),
-                      Text('Olá, Nome',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: 600,
-            width: MediaQuery.of(context).size.width - 20,
-            margin: const EdgeInsets.only(left: 10.0, right: 10.0),
-            // color: Colors.blue,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Tela Criar Receita',
-                  style: TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 600,
-            width: MediaQuery.of(context).size.width - 20,
-            margin: const EdgeInsets.only(left: 10.0, right: 10.0),
-            // color: Colors.red,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -266,52 +143,6 @@ class HomePageState extends State<HomePage> {
       ),
     );
   }
-
-  Widget buildAndroidActionSheet(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        ListTile(
-          leading: Icon(Icons.delete),
-          title: Text('Apagar'),
-          onTap: () => Navigator.pop(context, pickImage(null)),
-        ),
-        ListTile(
-          leading: Icon(Icons.camera_alt),
-          title: Text('Camera'),
-          onTap: () => Navigator.pop(context, pickImage(ImageSource.camera)),
-        ),
-        ListTile(
-          leading: Icon(Icons.image),
-          title: Text('Galeria'),
-          onTap: () => Navigator.pop(context, pickImage(ImageSource.gallery)),
-        ),
-      ],
-    );
-  }
-
-  Widget buildIOSActionSheet(BuildContext context) => CupertinoActionSheet(
-        actions: [
-          CupertinoActionSheetAction(
-            onPressed: () => Navigator.pop(context, pickImage(null)),
-            child: Text('Apagar', style: TextStyle(color: Colors.red)),
-          ),
-          CupertinoActionSheetAction(
-            onPressed: () =>
-                Navigator.pop(context, pickImage(ImageSource.camera)),
-            child: Text('Tirar Foto'),
-          ),
-          CupertinoActionSheetAction(
-            onPressed: () =>
-                Navigator.pop(context, pickImage(ImageSource.gallery)),
-            child: Text('Escolher Foto'),
-          ),
-        ],
-        cancelButton: CupertinoActionSheetAction(
-          child: Text('Cancelar'),
-          onPressed: () => Navigator.pop(context),
-        ), // CupertinoActionSheetAction
-      );
 }
 
 class CustomSwitch extends StatelessWidget {
